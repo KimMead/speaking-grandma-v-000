56bda787d1b5108b7b2f1a0f4d2ea20d7ec22c70
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-16 00:15:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "I LOVE YOU GRANDMA!.upcase"
+      return "I LOVE YOU TOO PUMPKIN!.upcase"
   else phrase == phrase.capitalize
   return "HUH?! SPEAK UP, SONNY!"
   end
